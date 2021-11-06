18f0927c524cfd3ebad42ee0a415dac1ce1cc8ac
aliosmanakca <akcaaliosman@gmail.com>
2021-11-06 14:12:57 -0400

    .

--- src/test/java/stepdefinitions/amazonStepDefinitions.java
+++ src/test/java/stepdefinitions/amazonStepDefinitions.java
@@ -1,5 +1,7 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -46,6 +48,34 @@ public class amazonStepDefinitions {
     @Then("sonucun ipad icerdigini test eder")
     public void sonucun_ipad_icerdigini_test_eder() {
         Assert.assertTrue(amazonPage.sonucYazisiElementi.getText().contains("ipad"));
+    }
+    @And("{string} icin arama yapar")
+    public void icinAramaYapar(String arananKelime) {
+
+        amazonPage.aramaKutusu.sendKeys(arananKelime+ Keys.ENTER);
+    }
+
+    @Then("sonucun {string} icerdigini test eder")
+    public void sonucunIcerdiginiTestEder(String arananKelime) {
+
+        Assert.assertTrue(amazonPage.sonucYazisiElementi.getText().contains(arananKelime));
+    }
+
+    @Given("kullanici {string} sayfasina gider")
+    public void kullaniciSayfasinaGider(String istenenUrl) {
+
+        Driver.getDriver().get(ConfigReader.getProperty(istenenUrl));
+    }
+
+    @And("sonuc sayisini yazdirir")
+    public void sonucSayisiniYazdirir() {
+        System.out.println(amazonPage.sonucYazisiElementi.getText());
+    }
+
+    @Then("basligin {string} icerdigini test eder")
+    public void basliginIcerdiginiTestEder(String arananKelime) {
+
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(arananKelime));
     }
 
 }
